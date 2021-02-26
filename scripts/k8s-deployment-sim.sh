b4b7656ca61c5ab62207db4838641633d076cb36
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-02-26 09:49:33 -0800

    Update k8s-deployment-sim.sh

--- scripts/k8s-deployment-sim.sh
+++ scripts/k8s-deployment-sim.sh
@@ -28,19 +28,25 @@ printf "\n\n\n*** [2.1] verify installation\n"
 kubectl get deployments && kubectl get pods && kubectl get services
 printf "\n\n\n*** [2.2] verify deployment spec\n"
 kubectl describe deployments $(kubectl get deployments|grep agent-plg|awk '{print $1}'|head -n 1)
-printf "\n\n\n*** [2.3] verify service spec\n"
-kubectl describe services $(kubectl get services|grep agent-plg|awk '{print $1}'|head -n 1)
-#printf "\n\n\n*** done debug go ahead delete all.\n\n"
-printf "\n\n\n*** [2.4] clear installation\n"
+printf "\n\n\n*** [2.3] clear installation\n"
 kubectl delete --all deployments && kubectl delete --all pods && kubectl delete --all services
 
-: 'printf "\n\n\n*** install client with remote vln template in minikube\n\n"
+printf "\n\n\n*** [3] install client with remote vln template in minikube\n"
 yq e '.global.agtK8Config.withPlugins.tls.enabled = false' -i k8s/example/values.yaml
 yq e '.global.agtK8Config.withPlugins.vln.enabled = true' -i k8s/example/values.yaml
 yq e '.global.agtK8Config.withPlugins.vln.remote = true' -i k8s/example/values.yaml
 helm install k8s/example --set-file global.agtConfig=k8s/example/client+vln.env --generate-name
 
-printf "\n\n\n*** verify logs in minikube\n\n"
+printf "\n\n\n*** [3.1] verify installation\n"
+kubectl get deployments && kubectl get pods && kubectl get services
+printf "\n\n\n*** [3.2] verify deployment spec\n"
+kubectl describe deployments $(kubectl get deployments|grep agent-plg|awk '{print $1}'|head -n 1)
+printf "\n\n\n*** [3.3] verify service spec\n"
+kubectl describe services $(kubectl get services|grep agent-plg|awk '{print $1}'|head -n 1)
+printf "\n\n\n*** [3.4] clear installation\n"
+kubectl delete --all deployments && kubectl delete --all pods && kubectl delete --all services
+
+: 'printf "\n\n\n*** verify logs in minikube\n\n"
 kubectl get deployments && kubectl get pods && kubectl get services && kubectl get ingresses
 #kubectl logs -p $(kubectl get pods|grep agent-plg|awk '{print $1}'|head -n 1) --since=5m
 kubectl rollout status deploy/$(kubectl get deployments|grep agent-plg|awk '{print $1}'|head -n 1)
