68514a73002bc2c451686ed5da9b61db22f26f07
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-02-25 21:23:57 -0800

    Update k8s-deployment-sim.sh

--- scripts/k8s-deployment-sim.sh
+++ scripts/k8s-deployment-sim.sh
@@ -5,14 +5,14 @@ yq e '.global.agtK8Config.withPlugins.tls.enabled = true' -i k8s/example/values.
 yq e '.global.agtK8Config.withPlugins.vln.enabled = false' -i k8s/example/values.yaml
 helm install k8s/example --set-file global.agtConfig=k8s/example/server+tls.env --generate-name
 
-printf "\n\n\n*** verify logs in minikube\n\n"
-kubectl get deployments && kubectl get pods && kubectl get services && kubectl get ingresses
+printf "\n\n\n*** verify logs in k8s\n\n"
+kubectl get deployments && kubectl get pods && kubectl get services
 kubectl rollout status deploy/$(kubectl get deployments|grep agent-plg|awk '{print $1}'|head -n 1)
 sleep 2
 kubectl logs -p $(kubectl get pods|grep agent-plg|awk '{print $1}'|head -n 1) --since=5m
 #kubectl describe deployments $(kubectl get pods|grep agent-plg|awk '{print $1}'|head -n 1)
 printf "\n\n\n*** done debug go ahead delete all.\n\n"
-kubectl delete --all deployments && kubectl delete --all pods && kubectl delete --all services && kubectl delete --all ingresses
+kubectl delete --all deployments && kubectl delete --all pods && kubectl delete --all services
 
 printf "\n\n\n*** install client with local vln multi-contr template in minikube\n\n"
 yq e '.global.agtK8Config.withPlugins.tls.enabled = false' -i k8s/example/values.yaml
