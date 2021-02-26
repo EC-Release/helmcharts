9e1eacd55c9f54e2014c7989b491f24d2ae92c03
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-02-25 22:23:26 -0800

    Update k8s-deployment-sim.sh

--- scripts/k8s-deployment-sim.sh
+++ scripts/k8s-deployment-sim.sh
@@ -7,14 +7,14 @@ helm install k8s/example --set-file global.agtConfig=k8s/example/server+tls.env 
 
 printf "\n\n\n*** verify logs in k8s\n\n"
 kubectl get deployments && kubectl get pods && kubectl get services
-kubectl rollout status deploy/$(kubectl get deployments|grep agent-plg|awk '{print $1}'|head -n 1)
-sleep 2
-kubectl logs -p $(kubectl get pods|grep agent-plg|awk '{print $1}'|head -n 1) --since=5m
-#kubectl describe deployments $(kubectl get pods|grep agent-plg|awk '{print $1}'|head -n 1)
-printf "\n\n\n*** done debug go ahead delete all.\n\n"
-kubectl delete --all deployments && kubectl delete --all pods && kubectl delete --all services
+#kubectl rollout status deploy/$(kubectl get deployments|grep agent-plg|awk '{print $1}'|head -n 1)
+#sleep 2
+#kubectl logs -p $(kubectl get pods|grep agent-plg|awk '{print $1}'|head -n 1) --since=5m
+kubectl describe deployments $(kubectl get deployments|grep agent-plg|awk '{print $1}'|head -n 1)
+#printf "\n\n\n*** done debug go ahead delete all.\n\n"
+#kubectl delete --all deployments && kubectl delete --all pods && kubectl delete --all services
 
-printf "\n\n\n*** install client with local vln multi-contr template in minikube\n\n"
+: 'printf "\n\n\n*** install client with local vln multi-contr template in minikube\n\n"
 yq e '.global.agtK8Config.withPlugins.tls.enabled = false' -i k8s/example/values.yaml
 yq e '.global.agtK8Config.withPlugins.vln.enabled = true' -i k8s/example/values.yaml
 yq e '.global.agtK8Config.withPlugins.vln.remote = false' -i k8s/example/values.yaml
@@ -48,4 +48,4 @@ kubectl get deployments && kubectl get pods && kubectl get services && kubectl g
 kubectl rollout status deploy/$(kubectl get deployments|grep agent-plg|awk '{print $1}'|head -n 1)
 #kubectl describe deployments $(kubectl get pods|grep agent-plg|awk '{print $1}'|head -n 1)
 printf "\n\n\n*** done debug go ahead delete all.\n\n"
-kubectl delete --all deployments && kubectl delete --all pods && kubectl delete --all services && kubectl delete --all ingresses
+kubectl delete --all deployments && kubectl delete --all pods && kubectl delete --all services && kubectl delete --all ingresses'
