701cc734c54192e00088f0ac716958e977378520
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-02-26 09:23:36 -0800

    Update k8s-deployment-sim.sh

--- scripts/k8s-deployment-sim.sh
+++ scripts/k8s-deployment-sim.sh
@@ -10,9 +10,9 @@ kubectl get deployments && kubectl get pods && kubectl get services
 #kubectl rollout status deploy/$(kubectl get deployments|grep agent-plg|awk '{print $1}'|head -n 1)
 #sleep 2
 #kubectl logs -p $(kubectl get pods|grep agent-plg|awk '{print $1}'|head -n 1) --since=5m
-printf "\n\n\n*** 1) verify deployment spec\n"
+printf "\n\n\n*** 1.1) verify deployment spec\n"
 kubectl describe deployments $(kubectl get deployments|grep agent-plg|awk '{print $1}'|head -n 1)
-printf "\n\n\n*** 1) verify service spec\n"
+printf "\n\n\n*** 1.2) verify service spec\n"
 kubectl describe services $(kubectl get services|grep agent-plg|awk '{print $1}'|head -n 1)
 #printf "\n\n\n*** done debug go ahead delete all.\n\n"
 #kubectl delete --all deployments && kubectl delete --all pods && kubectl delete --all services
