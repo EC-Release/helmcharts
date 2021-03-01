ed00983f26bbb265ee28d548d4a8f89cb0c924cb
Rama Rao Srikakulapu <RamaRao.Srikakulapu@ge.com>
2021-03-01 13:18:13 -0500

    k8 sim lb - install with name

--- scripts/k8s-deployment-sim.sh
+++ scripts/k8s-deployment-sim.sh
@@ -3,7 +3,7 @@
 printf "\n\n\n*** [0] install server with tls template and HA in k8s\n"
 yq e '.global.agtK8Config.withPlugins.tls.enabled = true' -i k8s/agent+lber/values.yaml
 yq e '.global.agtK8Config.withPlugins.vln.enabled = false' -i k8s/agent+lber/values.yaml
-helm install k8s/agent+lber --set-file global.agtConfig=k8s/example/server+tls.env --generate-name
+helm install my-app k8s/agent+lber --set-file global.agtConfig=k8s/example/server+tls.env
 printf "\n\n\n*** [0.1] verify installation\n"
 kubectl get deployments && kubectl get pods && kubectl get services && kubectl get ingress
 printf "\n\n\n*** [0.2] verify deployment spec\n"
