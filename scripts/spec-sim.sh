608bd051505c19a379ee7d1849ccf6c3521e61ed
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-03-05 10:28:54 -0800

    Update spec-sim.sh

--- scripts/spec-sim.sh
+++ scripts/spec-sim.sh
@@ -12,35 +12,35 @@ eval "sed -i -e 's#<AGENT_PLG_CHART_REV>#${AGENT_PLG_CHART_REV}#g' k8s/agent+plg
 eval "sed -i -e 's#<AGENT_HELPER_CHART_REV>#${AGENT_HELPER_CHART_REV}#g' k8s/agent+plg/Chart.yaml"
 eval "sed -i -e 's#<AGENT_LBER_CHART_REV>#${AGENT_LBER_CHART_REV}#g' k8s/agent+lber/Chart.yaml"
 eval "sed -i -e 's#<AGENT_HELPER_CHART_REV>#${AGENT_HELPER_CHART_REV}#g' k8s/agent+lber/Chart.yaml"
-eval "sed -i -e 's#<AGENT_HELPER_CHART_REV>#${AGENT_HELPER_CHART_REV}#g' k8s/example/Chart.yaml"
-eval "sed -i -e 's#<AGENT_PLG_CHART_REV>#${AGENT_PLG_CHART_REV}#g' k8s/example/Chart.yaml"
-# eval "sed -i -e 's#<AGENT_LBER_CHART_REV>#${AGENT_LBER_CHART_REV}#g' k8s/example/Chart.yaml"
-eval "sed -i -e 's#<AGENT_CHART_REV>#${AGENT_CHART_REV}#g' k8s/example/Chart.yaml"
-cat k8s/agent+helper/Chart.yaml k8s/agent/Chart.yaml k8s/agent+plg/Chart.yaml k8s/agent+lber/Chart.yaml k8s/example/Chart.yaml
+eval "sed -i -e 's#<AGENT_HELPER_CHART_REV>#${AGENT_HELPER_CHART_REV}#g' k8s/examples/agent/Chart.yaml"
+eval "sed -i -e 's#<AGENT_PLG_CHART_REV>#${AGENT_PLG_CHART_REV}#g' k8s/examples/agent/Chart.yaml"
+# eval "sed -i -e 's#<AGENT_LBER_CHART_REV>#${AGENT_LBER_CHART_REV}#g' k8s/examples/agent/Chart.yaml"
+eval "sed -i -e 's#<AGENT_CHART_REV>#${AGENT_CHART_REV}#g' k8s/examples/agent/Chart.yaml"
+cat k8s/agent+helper/Chart.yaml k8s/agent/Chart.yaml k8s/agent+plg/Chart.yaml k8s/agent+lber/Chart.yaml k8s/examples/agent/Chart.yaml
 
 printf "\n\n\n*** update server+tls.env \n"
-eval "sed -i -e 's#{{EC_TEST_OA2}}#${EC_TEST_OA2}#g' k8s/example/server+tls.env"
-eval "sed -i -e 's#{{EC_TEST_AID}}#${EC_TEST_AID}#g' k8s/example/server+tls.env"
-eval "sed -i -e 's#{{EC_TEST_TKN}}#${EC_TEST_TKN}#g' k8s/example/server+tls.env"
-eval "sed -i -e 's#{{EC_TEST_SST}}#${EC_TEST_SST}#g' k8s/example/server+tls.env"
-eval "sed -i -e 's#{{EC_TEST_HST}}#${EC_TEST_HST}#g' k8s/example/server+tls.env"
-eval "sed -i -e 's#{{EC_TEST_ZON}}#${EC_TEST_ZON}#g' k8s/example/server+tls.env"
-eval "sed -i -e 's#{{EC_TEST_GRP}}#${EC_TEST_GRP}#g' k8s/example/server+tls.env"
-eval "sed -i -e 's#{{EC_TEST_CID}}#${EC_TEST_CID}#g' k8s/example/server+tls.env"
-eval "sed -i -e 's#{{EC_TEST_CSC}}#${EC_TEST_CSC}#g' k8s/example/server+tls.env"
+eval "sed -i -e 's#{{EC_TEST_OA2}}#${EC_TEST_OA2}#g' k8s/examples/agent/server+tls.env"
+eval "sed -i -e 's#{{EC_TEST_AID}}#${EC_TEST_AID}#g' k8s/examples/agent/server+tls.env"
+eval "sed -i -e 's#{{EC_TEST_TKN}}#${EC_TEST_TKN}#g' k8s/examples/agent/server+tls.env"
+eval "sed -i -e 's#{{EC_TEST_SST}}#${EC_TEST_SST}#g' k8s/examples/agent/server+tls.env"
+eval "sed -i -e 's#{{EC_TEST_HST}}#${EC_TEST_HST}#g' k8s/examples/agent/server+tls.env"
+eval "sed -i -e 's#{{EC_TEST_ZON}}#${EC_TEST_ZON}#g' k8s/examples/agent/server+tls.env"
+eval "sed -i -e 's#{{EC_TEST_GRP}}#${EC_TEST_GRP}#g' k8s/examples/agent/server+tls.env"
+eval "sed -i -e 's#{{EC_TEST_CID}}#${EC_TEST_CID}#g' k8s/examples/agent/server+tls.env"
+eval "sed -i -e 's#{{EC_TEST_CSC}}#${EC_TEST_CSC}#g' k8s/examples/agent/server+tls.env"
 
 printf "\n\n\n*** update client+vln.env \n"
-eval "sed -i -e 's#{{EC_TEST_OA2}}#${EC_TEST_OA2}#g' k8s/example/client+vln.env"
-eval "sed -i -e 's#{{EC_TEST_ZON}}#${EC_TEST_ZON}#g' k8s/example/client+vln.env"
-eval "sed -i -e 's#{{EC_TEST_GRP}}#${EC_TEST_GRP}#g' k8s/example/client+vln.env"
-eval "sed -i -e 's#{{EC_TEST_CID}}#${EC_TEST_CID}#g' k8s/example/client+vln.env"
-eval "sed -i -e 's#{{EC_TEST_CSC}}#${EC_TEST_CSC}#g' k8s/example/client+vln.env"
+eval "sed -i -e 's#{{EC_TEST_OA2}}#${EC_TEST_OA2}#g' k8s/examples/agent/client+vln.env"
+eval "sed -i -e 's#{{EC_TEST_ZON}}#${EC_TEST_ZON}#g' k8s/examples/agent/client+vln.env"
+eval "sed -i -e 's#{{EC_TEST_GRP}}#${EC_TEST_GRP}#g' k8s/examples/agent/client+vln.env"
+eval "sed -i -e 's#{{EC_TEST_CID}}#${EC_TEST_CID}#g' k8s/examples/agent/client+vln.env"
+eval "sed -i -e 's#{{EC_TEST_CSC}}#${EC_TEST_CSC}#g' k8s/examples/agent/client+vln.env"
 
 printf "\n\n\n*** update gateway.env \n"
-eval "sed -i -e 's#{{EC_TEST_ZON}}#${EC_TEST_ZON}#g' k8s/example/gateway.env"
-eval "sed -i -e 's#{{EC_TEST_GRP}}#${EC_TEST_GRP}#g' k8s/example/gateway.env"
-eval "sed -i -e 's#{{EC_TEST_SST}}#${EC_TEST_SST}#g' k8s/example/gateway.env"
-eval "sed -i -e 's#{{EC_TEST_TKN}}#${EC_TEST_TKN}#g' k8s/example/gateway.env"
+eval "sed -i -e 's#{{EC_TEST_ZON}}#${EC_TEST_ZON}#g' k8s/examples/agent/gateway.env"
+eval "sed -i -e 's#{{EC_TEST_GRP}}#${EC_TEST_GRP}#g' k8s/examples/agent/gateway.env"
+eval "sed -i -e 's#{{EC_TEST_SST}}#${EC_TEST_SST}#g' k8s/examples/agent/gateway.env"
+eval "sed -i -e 's#{{EC_TEST_TKN}}#${EC_TEST_TKN}#g' k8s/examples/agent/gateway.env"
 
 printf "\n\n\n*** packaging w/ dependencies (ec-release/oci) \n"
 mkdir -p k8s/pkg/agent/$AGENT_CHART_REV k8s/pkg/agent+helper/$AGENT_HELPER_CHART_REV k8s/pkg/agent+plg/$AGENT_PLG_CHART_REV k8s/pkg/agent+lber/$AGENT_LBER_CHART_REV
@@ -55,28 +55,28 @@ helm package k8s/agent+plg -d k8s/pkg/agent+plg/$AGENT_PLG_CHART_REV
 helm package k8s/agent+lber -d k8s/pkg/agent+lber/$AGENT_LBER_CHART_REV
 
 printf "update dependencies in example chart for test"
-helm dependency update k8s/example
-ls -la k8s/example/charts/
+helm dependency update k8s/examples/agent
+ls -la k8s/examples/agent/charts/
 
 printf "\n\n\n*** test server with tls template\n"
-yq e '.global.agtK8Config.withPlugins.tls.enabled = true' -i k8s/example/values.yaml
-yq e '.global.agtK8Config.withPlugins.vln.enabled = false' -i k8s/example/values.yaml
-helm template k8s/example --debug --set-file global.agtConfig=k8s/example/server+tls.env
+yq e '.global.agtK8Config.withPlugins.tls.enabled = true' -i k8s/examples/agent/values.yaml
+yq e '.global.agtK8Config.withPlugins.vln.enabled = false' -i k8s/examples/agent/values.yaml
+helm template k8s/examples/agent --debug --set-file global.agtConfig=k8s/examples/agent/server+tls.env
 
 printf "\n\n\n*** test client with local vln template\n"
-yq e '.global.agtK8Config.withPlugins.tls.enabled = false' -i k8s/example/values.yaml
-yq e '.global.agtK8Config.withPlugins.vln.enabled = true' -i k8s/example/values.yaml
-yq e '.global.agtK8Config.withPlugins.vln.remote = false' -i k8s/example/values.yaml
-helm template k8s/example --debug --set-file global.agtConfig=k8s/example/client+vln.env
+yq e '.global.agtK8Config.withPlugins.tls.enabled = false' -i k8s/examples/agent/values.yaml
+yq e '.global.agtK8Config.withPlugins.vln.enabled = true' -i k8s/examples/agent/values.yaml
+yq e '.global.agtK8Config.withPlugins.vln.remote = false' -i k8s/examples/agent/values.yaml
+helm template k8s/examples/agent --debug --set-file global.agtConfig=k8s/examples/agent/client+vln.env
 
 printf "\n\n\n*** test client with remote vln template\n"
-yq e '.global.agtK8Config.withPlugins.tls.enabled = false' -i k8s/example/values.yaml
-yq e '.global.agtK8Config.withPlugins.vln.enabled = true' -i k8s/example/values.yaml
-yq e '.global.agtK8Config.withPlugins.vln.remote = true' -i k8s/example/values.yaml
-helm template k8s/example --debug --set-file global.agtConfig=k8s/example/client+vln.env
+yq e '.global.agtK8Config.withPlugins.tls.enabled = false' -i k8s/examples/agent/values.yaml
+yq e '.global.agtK8Config.withPlugins.vln.enabled = true' -i k8s/examples/agent/values.yaml
+yq e '.global.agtK8Config.withPlugins.vln.remote = true' -i k8s/examples/agent/values.yaml
+helm template k8s/examples/agent --debug --set-file global.agtConfig=k8s/examples/agent/client+vln.env
 
 printf "\n\n\n*** test gateway agt template\n"
-helm template k8s/example --debug --set-file global.agtConfig=k8s/example/gateway.env
+helm template k8s/examples/agent --debug --set-file global.agtConfig=k8s/examples/agent/gateway.env
 
 printf "\n\n\n*** pkg indexing (ec-release/oci)\n"
 #https://raw.githubusercontent.com/EC-Release/helmcharts/disty/agent/0.1.7
